c89e710041dcf3f655361222dcb8546e68bff219
PV1311 <vermapranjal35@gmail.com>
2024-02-23 03:57:43 +0530

    new commit

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-document.querySelector(".install-app").addEventListener("click", () => {
-    window.location.href = "./assets/spotify-1-2-31-1205.exe"
-})
-search = document.querySelector(".Search")
-search.addEventListener("click", (event) => {
-    document.querySelector(".Search>input").style.display = "inline"
-    document.querySelector(".Search>a").style.display = "none"
-    document.querySelector(".Search>button").addEventListener("click", () => {
-        value = document.querySelector(".Search>input").value;
-        if (value != "") console.log(value);
-        document.querySelector(".Search>input").value = ""
-    })
-})
+
 
 
 cards = document.querySelectorAll(".card")
@@ -21,7 +9,3 @@ for (let card of cards) {
     })
 }
 
-icon1 = document.querySelector(".icon1")
-icon1.addEventListener("click", () => {
-    icon1.style.backgroundColor="green";
-})
